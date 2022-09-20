1dd865e3df5d6d8c93983dd0065a759c7b3516d0
Khiljii <hamadraza047@github.com>
2022-09-20 12:42:36 +0500

    task 2

--- Student.java
+++ Student.java
@@ -6,20 +6,60 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 class Record{
-    String name;
+    String firstName;
+
+    String lastName;
+
+    String age;
     String className;
-    String rolNo;
-    public Record(String n,String c,String r){
-        this.name=n;
-        this.className=c;
-        this.rolNo=r;
+    String rollNo;
+    Scanner sn=new Scanner(System.in);
+    public void setData(){
+
+        System.out.print("Enter Studdent First Name: ");
+        firstName=sn.nextLine();
+
+        System.out.print("Enter Studdent Last Name: ");
+        lastName=sn.nextLine();
+
+        System.out.print("Enter Studdent Age: ");
+        age=sn.nextLine();
+
+        System.out.print("Enter Studdent Class: ");
+        className=sn.nextLine();
+
+        System.out.print("Enter Studdent Roll No: ");
+        rollNo=sn.nextLine();
+        System.out.println("");
+
+    }
+    public List<Record> search(List<Record> r){
+        System.out.print("Enter to search: ");
+        String name=sn.nextLine();
+        System.out.println("");
+        List<Record> students=r.stream().filter(x -> x.firstName.equals(name)).collect(Collectors.toList());
+        List<Record> student1=r.stream().filter(x -> x.lastName.equals(name)).collect(Collectors.toList());
+        List<Record> student2=r.stream().filter(x -> x.age.equals(name)).collect(Collectors.toList());
+        List<Record> student3=r.stream().filter(x -> x.className.equals(name)).collect(Collectors.toList());
+        List<Record> student4=r.stream().filter(x -> x.rollNo.equals(name)).collect(Collectors.toList());
+        if(students.size()>0){
+            return students;
+        }else if (student1.size()>0){
+            return student1;
+        } else if (student2.size()>0) {
+            return student2;
+        } else if (student3.size()>0) {
+            return student3;
+        }else
+            return student4;
+
     }
 
     @Override
     public String toString() {
-        return "Name: '" + name +
+        return "FirstName: '" + firstName +"', LastName: '" + lastName +"', Age: '" + age +
                 "', Class: '" + className +
-                "', Roll No: '" + rolNo + "'";
+                "', Roll No: '" + rollNo + "'";
     }
 }
 
@@ -30,23 +70,14 @@ public class Student {
         List<Record> recordList= new ArrayList<>();
         String check;
         do{
-            System.out.print("Enter Studdent Name: ");
-            String name=sn.nextLine();
-            System.out.println("");
-            System.out.print("Enter Studdent Class: ");
-            String className=sn.nextLine();
-            System.out.println("");
-            System.out.print("Enter Studdent Roll No: ");
-            String rollNo=sn.nextLine();
-            System.out.println("");
-            recordList.add(new Record(name,className,rollNo));
+            Record record=new Record();
+            record.setData();
+            recordList.add(record);
             System.out.println("Enter new Student Record Y/N");
             check=sn.nextLine();
-        }while (check.equals('Y') || check.equals('y'));
-        System.out.print("Enter name to search: ");
-        String name=sn.nextLine();
-        System.out.println("");
-        List<Record> students = recordList.stream().filter(x -> x.name.equals(name)).collect(Collectors.toList());
+        }while (check.equals('N') || check.equals('n'));
+        Record record=new Record();
+                List<Record> students = record.search(recordList);
         if(students.size()==0){
             System.out.println("Student not found");
         }else {
